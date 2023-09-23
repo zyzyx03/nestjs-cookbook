8a4ab23dda54a0a44f20f36cd721444472528d49
fauzuwan.nazri93@gmail.com <fyzyx@pop-os.localdomain>
2023-09-23 21:40:58 +0800

    modify controller

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get, Post, Put, Delete, Param, Query} from '@nestjs/common';
+import { query } from 'express';
+import { type } from 'os';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+    
+    // GET /ninjas?type=fast --> []
+    @Get()
+    getNinjas(@Query('type') type: string){
+        return [{ type }]
+    }
+    // GET /ninjas/:id --> { ... }
+    @Get(':id')
+    getOneNinja(@Param('id') id: string){
+        return {
+            id,
+        }
+    }
+    // POST /ninjas/:id
+    @Post()
+    createNinja(){
+        return {}
+    }
+    // PUT /ninjas/:id --> { ... }
+    @Put(':id')
+    updateNinja(){
+        return {}
+    }
+    // DELETE /ninjas/:id
+    @Delete(':id')
+    removeNinja(){
+        return {}
+    }
+
+}
+
