3f2f534eceaf5720756c9672731978d4c54ee2b1
fauzuwan.nazri93@gmail.com <fyzyx@pop-os.localdomain>
2023-09-23 23:27:27 +0800

    finishing controller with put and post request

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,6 +1,8 @@
-import { Controller,Get, Post, Put, Delete, Param, Query} from '@nestjs/common';
+import { Controller,Get, Post, Put, Delete, Param, Query, Body} from '@nestjs/common';
 import { query } from 'express';
 import { type } from 'os';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -10,6 +12,7 @@ export class NinjasController {
     getNinjas(@Query('type') type: string){
         return [{ type }]
     }
+
     // GET /ninjas/:id --> { ... }
     @Get(':id')
     getOneNinja(@Param('id') id: string){
@@ -17,16 +20,24 @@ export class NinjasController {
             id,
         }
     }
+
     // POST /ninjas/:id
     @Post()
-    createNinja(){
-        return {}
+    createNinja(@Body() createNinjaDto: CreateNinjaDto){
+        return {
+            name: createNinjaDto.name
+        }
     }
+
     // PUT /ninjas/:id --> { ... }
     @Put(':id')
-    updateNinja(){
-        return {}
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto){
+        return {
+            id,
+            name: updateNinjaDto
+        }
     }
+
     // DELETE /ninjas/:id
     @Delete(':id')
     removeNinja(){
