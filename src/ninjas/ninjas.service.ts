5588736edfd9c03072f1a5a5130f9597256f64ff
fauzuwan.nazri93@gmail.com <fyzyx@pop-os.localdomain>
2023-09-24 01:27:29 +0800

    add getNinjas Method

--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+    private ninjas = [
+        {id:0, name: 'ninjaA', weapon: 'stars'},
+        {id:1, name: 'ninjaB', weapon: 'nunchucks'},
+    ]
+
+    /*This method getNinjas receive optional argument wither 'stars', 
+    'nunchucks' or none */
+    getNinjas(weapon?: 'stars' | 'nunchucks'){
+        if(weapon){
+            return this.ninjas.filter((ninja) => ninja.weapon === weapon)
+        }
+
+        return this.ninjas
+    }
+}
