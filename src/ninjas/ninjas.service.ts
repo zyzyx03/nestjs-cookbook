2a1889469d66e9e899b959d8425a38ad4b2e413f
zyzyx03 <fauzuwan.nazri93@gmail.com>
2023-09-24 14:48:39 +0800

    adding crud login in services

--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { error } from 'console';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
+/* @Injectable this is the decorator that incharge in instantiate 
+this class so that it can automatically inject to anyhting that depend on it
+*/
 @Injectable()
 export class NinjasService {
     private ninjas = [
@@ -7,7 +13,7 @@ export class NinjasService {
         {id:1, name: 'ninjaB', weapon: 'nunchucks'},
     ]
 
-    /*This method getNinjas receive optional argument wither 'stars', 
+    /*This method getNinjas receive optional argument with 'stars', 
     'nunchucks' or none */
     getNinjas(weapon?: 'stars' | 'nunchucks'){
         if(weapon){
@@ -15,5 +21,44 @@ export class NinjasService {
         }
 
         return this.ninjas
+    }
+
+    getNinja(id: number){
+        const ninja = this.ninjas.find((ninja) => ninja.id === id)
+        if(!ninja){
+            throw new Error('ninja not found')
+        }
+
+        return ninja
+    }
+
+    createNinja(createNinjaDto: CreateNinjaDto){
+        const newNinja = {
+            ...createNinjaDto,
+            id: Date.now()
+        }
+        this.ninjas.push(newNinja)
+
+        return newNinja
+    }
+
+    updateNinja(id: number, updateNinjaDto: UpdateNinjaDto){
+        this.ninjas = this.ninjas.map((ninja) => {
+            if (ninja.id === id) {
+                return { ...ninja, ...updateNinjaDto}
+            }
+
+            return ninja
+        })
+
+        return this.getNinja(id)
+    }
+
+    removeNinja(id: number){
+        const toBeRemoved =  this.getNinja(id)
+
+        this.ninjas = this.ninjas.filter((ninja) => ninja.id !== id)
+
+        return toBeRemoved
     }
 }
