2ea636799e8776d517859a0072ca8c981ce1fe1c
zyzyx03 <fauzuwan.nazri93@gmail.com>
2023-09-25 23:25:40 +0800

    add validation for weapon

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,4 +1,10 @@
+import { IsEnum, MinLength } from "class-validator"
+
+// Providing additional metadata to this class @MinLength
 export class CreateNinjaDto {
+    @MinLength(3)
     name: string
+
+    @IsEnum(['stars', 'nunchucks'], { message: 'Use correct weapon!'})
     weapon: 'stars' | 'nunchucks'
 }
